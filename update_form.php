4175e2d6eeefc9a8a18f570b070ef62d78f9ce57
Z0ro0 <chlrkdmf0805@gmail.com>
2022-09-22 10:19:30 +0900

    글 수정 화면에 데이터 불러오고 상자 안에 출력하기

--- update_form.php
+++ update_form.php
@@ -5,8 +5,8 @@ $query = "select * from bbs where id=$idx";
 $result = mysqli_query($conn,$query);
 $re = mysqli_fetch_row($result);
 echo "$re[0] $re[1] $re[2] $re[3] $re[4] $re[5] $re[6]";
-?>
 
+?>
 
 
 <!doctype html>
@@ -22,27 +22,27 @@ echo "$re[0] $re[1] $re[2] $re[3] $re[4] $re[5] $re[6]";
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>BBS</h1>
-    <form method="post" action="write.php">
+    <h1>BBS 수정</h1>
+    <form method="post" action="update_form.php">
     <div class="form-floating mb-3 col-sm-3">
-        <label for="exampleFormControlInput1" class="form-label">Title</label>
-        <input type="title" class="form-control" id="exampleFormControlInput1" name="title" placeholder="title">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+        Title<input type="title" class="form-control" id="exampleFormControlInput1" name="title" placeholder="title" value="<?php echo $re[1] ?>">
       </div>
       <div class="form-floating mb-3 col-sm-3">
-        <label for="exampleFormControlInput1" class="form-label">Writer</label>
-        <input type="writer" class="form-control" id="exampleFormControlInput1" name="writer" placeholder="writer">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+        Writer<input type="writer" class="form-control" id="exampleFormControlInput1" name="writer" placeholder="writer" value="<?php echo $re[2] ?>">
       </div>
       <div class="form-floating mb-3 col-sm-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" name= "email" placeholder="email">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+        Email address<input type="email" class="form-control" id="exampleFormControlInput1" name= "email" placeholder="email" value="<?php echo $re[3] ?>">
       </div>
       <div class="form-floating mb-3 col-sm-3">
-        <label for="exampleFormControlInput1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleFormControlInput1" name= "password" placeholder="email">
+        <label for="exampleFormControlInput1" class="form-label"></label>
+        Password<input type="password" class="form-control" id="exampleFormControlInput1" name= "password" placeholder="email" value="<?php echo $re[4] ?>">
       </div>
       <div class="form-floating mb-3 col-sm-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Content</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" name="content" rows="3"></textarea>
+        <label for="exampleFormControlTextarea1" class="form-label"></label>
+        Content<textarea class="form-control" id="exampleFormControlTextarea1" name="content" rows="3"><?php echo $re[5] ?></textarea>
       </div>
       <button type="submit" class="btn btn-warning">수정완료</button>
     </form>
